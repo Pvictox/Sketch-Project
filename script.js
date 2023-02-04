63d7231e4173c4264d0a3bdb738c3fb841187d37
Pedro <paf.victor@gmail.com>
2023-02-04 17:32:09 -0300

    Fixing size of grid and itens layout

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const gridContainer = document.querySelector("#grid");
 
 
-let fillContainer = (dimension = 2) => {
+let fillContainer = (dimension = 30) => {
     //100% / quant de blocos por linha
     let flexBasis_Value = 100/dimension;
+    let gridWidth = gridContainer.offsetWidth;
+    let gridHeight = gridContainer.offsetHeight;
     for(let i =0; i<Math.pow(dimension,2); i++){
         let newDiv = document.createElement("div");
         newDiv.id = i;
         newDiv.classList.add("cell");
+        newDiv.style.width = (gridWidth/dimension).toString()+"px";
+        newDiv.style.height = (gridHeight/dimension).toString()+"px";
         newDiv.style.flexGrow = 1;
         newDiv.style.flexShrink = 0;
         newDiv.style.flexBasis = flexBasis_Value.toString()+"%";
